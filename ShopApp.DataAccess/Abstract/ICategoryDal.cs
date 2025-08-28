033ea8c2647a755c4f28a365bb6417eb198625ad
metndurn <mtndrn123@gmail.com>
2025-08-28 19:14:12 +0300

    Repository 1 işlemleri yapıldı interfaceler eklendi içindeki metodlara implemente yapıldı

---
+++ ShopApp.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,20 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Abstract
+{
+	public interface ICategoryDal
+	{
+		Category GetById(int id);/*id ye göre Category getirecek*/
+		Category GetOne(Expression<Func<Category, bool>> filter);/*tek bir Category getirecek*/
+		IQueryable<Category> GetAll(Expression<Func<Category, bool>> filter);/*butun Categoryleri getirecek*/
+		void Add(Category entity);/*Category ekleme*/
+		void Update(Category entity);/*Category güncelleme*/
+		void Delete(Category entity);/*Category silme*/
+	}
+}
---
+++ ShopApp.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Abstract
+{
+	public interface IProductDal //metotları buraya yazacağız
+	{
+		Product GetById(int id);/*id ye göre ürünü getirecek*/
+		Product GetOne(Expression<Func<Product, bool>> filter);/*tek bir ürün getirecek*/
+		IQueryable<Product> GetAll(Expression<Func<Product, bool>> filter);/*butun ürünleri getirecek*/
+		void Add(Product entity);/*ürün ekleme*/
+		void Update(Product entity);/*ürün güncelleme*/
+		void Delete(Product entity);/*ürün silme*/
+	}
+}
