125291ca4d01d4e67f9df05f566d9fc0a355d4da
metndurn <mtndrn123@gmail.com>
2025-08-31 19:33:21 +0300

    program.cs ve dependency ınjection işlemi yapıldı

---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+
+namespace ShopApp.WebUI.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IProductService _productService;
+		public HomeController(IProductService productService)//dependency injection yani bağımlılık enjeksiyonu yapıyoruz
+		{
+			_productService = productService;
+		}
+
+		public IActionResult Index()
+		{
+			return View(_productService.GetAll());
+		}
+	}
+}
--- ShopApp.WebUI/Program.cs
+++ ShopApp.WebUI/Program.cs
@@ -1,6 +1,19 @@
+using ShopApp.Business.Abstract;
+using ShopApp.Business.Concrete;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IProductDal, MemoryProductDal>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddControllersWithViews();//mvc ekledik
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
