c9b859705f11eb4670f9d1f6f7c36ccef8031259
metndurn <mtndrn123@gmail.com>
2025-09-25 00:32:40 +0300

    migration ile veritabanını olusturduk

--- ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -12,9 +12,17 @@ namespace ShopApp.DataAccess.Concrete.EfCore
 	{
 		override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer(@"Server=MONSTER;Database=ShopDb;Trusted_Connection=True;");
+			optionsBuilder.UseSqlServer(@"Server=MONSTER;Database=ShopDb;Trusted_Connection=True;TrustServerCertificate=True;");
 		}
 
+		/*ıkı tane ıd kısmı var ve aynı yapı ıcındedır bunların hepsı tek tek geşmesin diye
+		 burada birleştirme yapılması lazım yanı productıd ve categoryıd sınıflarında ki
+		ıd lerden bahsedıyoruz bunlar aynı gelmelı amac birincil anahtarların ıkısının olmasını saglamak*/
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<ProductCategory>()
+				.HasKey(pc => new { pc.ProductId, pc.CategoryId });
+		}
 		public DbSet<Product> Products { get; set; }
 		public DbSet<Category> Categories { get; set; }
 	}
